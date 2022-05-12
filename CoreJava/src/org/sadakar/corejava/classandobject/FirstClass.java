9b037225d6c84550380812574b3b1340b69c0c1c
sadakar <sadakar.1988@gmail.com>
2022-05-12 14:51:35 +0530

    test commit2

--- CoreJava/src/org/sadakar/corejava/classandobject/FirstClass.java
+++
@@ -1,5 +0,0 @@
-package org.sadakar.corejava.classandobject;
-
-public class FirstClass {
-
-}
---
+++ CoreJava/src/org/sadakar/corejava/objectandclass/StudentMainMethodInsideClass.java
@@ -0,0 +1,32 @@
+// Program to define class and object
+// Object and Class Example: main within the class
+
+/* What is instance variable in java ? 
+A variable which is created inside the class but outside the method is known as an instance variable. 
+Instance variable doesn't get memory at compile time. 
+It gets memory at runtime when an object or instance is created. That is why it is known as an instance variable.
+*/
+package org.sadakar.corejava.objectandclass;
+
+public class StudentMainMethodInsideClass {
+
+	int id; // instance variable
+	String name; // instance variable
+
+	// creating main method inside the class
+
+	public static void main(String args[]) {
+
+		// Creating an object or instance
+
+		// s is reference variable 
+		// new StudentMainMethodInClass() is the object 
+		StudentMainMethodInsideClass s = new StudentMainMethodInsideClass(); 
+
+		// printing values of an object
+
+		System.out.println(s.id);
+		System.out.println(s.name);
+	}
+
+}
